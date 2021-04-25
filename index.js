33765a0a236ab0b083c1e46389dda1e4f0b0dfc8
WalukaUI <cwaluka@yahoo.com>
2021-04-24 18:29:08 -0700

    Refactoring

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const countiess = "https://data.mo.gov/resource/byps-gsbw.json",
     LawEnforcements = "https://data.mo.gov/resource/cgbu-k38b.json",
     ZipcodeDatas = "https://data.mo.gov/resource/8ejy-sj4q.json",
@@ -12,9 +11,7 @@ const countiess = "https://data.mo.gov/resource/byps-gsbw.json",
     serchvalue = document.getElementById('allSearch'),
     formBtn = document.getElementById('submits')
 
-
 //create main buttons 
-
 const populateButtons = () => {
     let jobsBtn = document.createElement('button'),
         jobCenters = document.createElement('button'),
@@ -39,17 +36,16 @@ const populateButtons = () => {
     LawEnforcement.innerHTML = 'Law Enforcement Agencies'
     LawEnforcement.value = LawEnforcements
     moUnemployment.innerHTML = 'MO Unemployment Rate'
-    moUnemployment.value=UnemploymentRates
+    moUnemployment.value = UnemploymentRates
     jobCenters.innerHTML = 'MO Job Centers'
-    jobCenters.value=mOjobCenters
+    jobCenters.value = mOjobCenters
     counties.innerHTML = 'Counties of MO'
-    counties.value=countiess
+    counties.value = countiess
     ZipcodeData.innerHTML = 'MO Zipcode Data'
-    ZipcodeData.value=ZipcodeDatas
+    ZipcodeData.value = ZipcodeDatas
 }
 
 //fletch  data 
-
 function getApiData(a) {
     fetch(`${a}`, {
         method: 'GET',
@@ -65,72 +61,93 @@ function getApiData(a) {
             Listener()
             j.forEach(e => {
                 if (a === GovernmentJob) {
-                    populateJobs(e)
+                    populateJobs(e, a)
                 } else if (a === FarmersMarketss) {
-                    populateFarmersdata(e)
+                    populateJobs(e, a)
                 } else if (a === LawEnforcements) {
                     populateLawEnforcements(e)
-                }else if(a===UnemploymentRates){
-                   populateUnemploymentRates(e)    
-                }else if(a===mOjobCenters){
+                } else if (a === UnemploymentRates) {
+                    populateUnemploymentRates(e)
+                } else if (a === mOjobCenters) {
                     populateJobCenters(e);
-                }else if(a===countiess){
+                } else if (a === countiess) {
                     populateCounties(e);
-                }else if(a===ZipcodeDatas){
+                } else if (a === ZipcodeDatas) {
                     populateZips(e);
                 }
-                
             });
         })
 }
 
-
 //Display governmentJob results on DOM
+// const populateJobs = (a) => {
+//     let div1 = document.createElement('div'),
+//         h3Tag = document.createElement('h3'),
+//         pTag = document.createElement('p'),
+//         h4Tag = document.createElement('h4')
 
-const populateJobs = (a) => {
+//     detailsSection.appendChild(div1)
+//     div1.appendChild(h3Tag)
+//     div1.appendChild(pTag)
+//     div1.appendChild(h4Tag)
+
+//     div1.className = 'moJobsDiv'
+//     h3Tag.innerHTML = a.title
+//     pTag.innerHTML = a.jobdescription
+//     pTag.maxlength = "10"
+//     h4Tag.innerHTML = `${a.salarybasis} : ${a.salarystart}`
+// }
+const populateJobs = (a, b) => {
     let div1 = document.createElement('div'),
         h3Tag = document.createElement('h3'),
         pTag = document.createElement('p'),
         h4Tag = document.createElement('h4')
 
     detailsSection.appendChild(div1)
-    div1.appendChild(h3Tag)
-    div1.appendChild(pTag)
-    div1.appendChild(h4Tag)
-
-    div1.className = 'moJobsDiv'
-    h3Tag.innerHTML = a.title
-    pTag.innerHTML = a.jobdescription
-    pTag.maxlength = "10"
-    h4Tag.innerHTML = `${a.salarybasis} : ${a.salarystart}`
+    let x = div1.appendChild(h3Tag)
+    let y = div1.appendChild(pTag)
+    let z = div1.appendChild(h4Tag)
+    if (b === GovernmentJob) {
+        div1.className = 'moJobsDiv'
+        h3Tag.innerHTML = a.title
+        pTag.innerHTML = a.jobdescription
+        pTag.maxlength = "10"
+        h4Tag.innerHTML = `${a.salarybasis} : ${a.salarystart}`
+    } else if (b === FarmersMarketss) {
+        div1.className = 'moFarmersDiv'
+        h3Tag.innerHTML = `City: ${a.city}`
+        pTag.innerHTML = `Company: ${a.business_name}`
+        pTag.maxlength = "10"
+        h4Tag.innerHTML = a.location_description
+    }
 }
+
 
 //Display Farmersdata results on DOM
-const populateFarmersdata = (a) => {
-    let div1 = document.createElement('div'),
-        h3Tag = document.createElement('h3'),
-        pTag = document.createElement('p'),
-        h4Tag = document.createElement('h4')
+// const populateFarmersdata = (a) => {
+//     let div1 = document.createElement('div'),
+//         h3Tag = document.createElement('h3'),
+//         pTag = document.createElement('p'),
+//         h4Tag = document.createElement('h4')
 
-    detailsSection.appendChild(div1)
-    div1.appendChild(h3Tag)
-    div1.appendChild(pTag)
-    div1.appendChild(h4Tag)
+//     detailsSection.appendChild(div1)
+//     div1.appendChild(h3Tag)
+//     div1.appendChild(pTag)
+//     div1.appendChild(h4Tag)
 
-    div1.className = 'moFarmersDiv'
-    h3Tag.innerHTML = `City: ${a.city}`
-    pTag.innerHTML = `Company: ${a.business_name}`
-    pTag.maxlength = "10"
-    h4Tag.innerHTML = a.location_description
-
-}
+//     div1.className = 'moFarmersDiv'
+//     h3Tag.innerHTML = `City: ${a.city}`
+//     pTag.innerHTML = `Company: ${a.business_name}`
+//     pTag.maxlength = "10"
+//     h4Tag.innerHTML = a.location_description
+// }
 
 //Display LawEnforcements results on DOM
 const populateLawEnforcements = (a) => {
     let div1 = document.createElement('div'),
         h3Tag = document.createElement('h3'),
         h4Tag1 = document.createElement('h4')
-        pTag = document.createElement('p'),
+    pTag = document.createElement('p'),
         h4Tag2 = document.createElement('h4')
 
     detailsSection.appendChild(div1)
@@ -141,16 +158,16 @@ const populateLawEnforcements = (a) => {
 
     div1.className = 'moLawDiv'
     h3Tag.innerHTML = a.name
-    h4Tag1.innerHTML=`City Name: ${a.city}`
+    h4Tag1.innerHTML = `City Name: ${a.city}`
     pTag.innerHTML = `Contact Number: ${a.voice_number}`
     h4Tag2.innerHTML = `Address : ${a.street_address_2}`
 }
 //Display Unemployment data of MO on DOM
-const populateUnemploymentRates=(a)=> {
+const populateUnemploymentRates = (a) => {
     let div1 = document.createElement('div'),
         h3Tag = document.createElement('h3'),
         h4Tag1 = document.createElement('h4')
-        pTag = document.createElement('p'),
+    pTag = document.createElement('p'),
         h3Tag2 = document.createElement('h3')
 
     detailsSection.appendChild(div1)
@@ -161,17 +178,17 @@ const populateUnemploymentRates=(a)=> {
 
     div1.className = 'moUnEmpDiv'
     h3Tag.innerHTML = a.area_name
-    h4Tag1.innerHTML=`Rate: ${a.unemployment_rate}`
+    h4Tag1.innerHTML = `Rate: ${a.unemployment_rate}`
     pTag.innerHTML = `Year: ${a.year}`
     h3Tag2.innerHTML = `Laborforce : ${a.laborforce}`
 }
 
 //Display Job Centers data of MO on DOM
-const populateJobCenters=(a)=> {
+const populateJobCenters = (a) => {
     let div1 = document.createElement('div'),
         h3Tag = document.createElement('h3'),
         h4Tag1 = document.createElement('h4')
-        pTag = document.createElement('p'),
+    pTag = document.createElement('p'),
         h3Tag2 = document.createElement('h3')
 
     detailsSection.appendChild(div1)
@@ -182,16 +199,16 @@ const populateJobCenters=(a)=> {
 
     div1.className = 'moJobCenDiv'
     h3Tag.innerHTML = a.city
-    h4Tag1.innerHTML=`Contact #: ${a.phone}`
+    h4Tag1.innerHTML = `Contact #: ${a.phone}`
     pTag.innerHTML = `Center Name: ${a.career_center_name}`
     h3Tag2.innerHTML = `Address : ${a.address1}`
 }
 //Display Counties data of MO on DOM
-const populateCounties=(a)=> {
+const populateCounties = (a) => {
     let div1 = document.createElement('div'),
         h3Tag = document.createElement('h3'),
         h4Tag1 = document.createElement('h4')
-        pTag = document.createElement('p'),
+    pTag = document.createElement('p'),
         h3Tag2 = document.createElement('h3')
 
     detailsSection.appendChild(div1)
@@ -202,16 +219,16 @@ const populateCounties=(a)=> {
 
     div1.className = 'moCountDiv'
     h3Tag.innerHTML = a.name
-    h4Tag1.innerHTML=`County Seat: ${a.cnty_seat}`
+    h4Tag1.innerHTML = `County Seat: ${a.cnty_seat}`
     pTag.innerHTML = `Code: ${a.fips}`
 }
 
 //Display Zipcode data of MO on DOM
-const populateZips=(a)=> {
+const populateZips = (a) => {
     let div1 = document.createElement('div'),
         h3Tag = document.createElement('h3'),
         h4Tag1 = document.createElement('h4')
-        pTag = document.createElement('p'),
+    pTag = document.createElement('p'),
         h3Tag2 = document.createElement('h3')
 
     detailsSection.appendChild(div1)
@@ -222,7 +239,7 @@ const populateZips=(a)=> {
 
     div1.className = 'moZipDiv'
     h3Tag.innerHTML = a.zcta5ce
-    h4Tag1.innerHTML=`Land Ac: ${a.aland}`
+    h4Tag1.innerHTML = `Land Ac: ${a.aland}`
     pTag.innerHTML = `Latitude: ${a.centroid.latitude} longitude: ${a.centroid.longitude}`
 }
 
@@ -235,9 +252,9 @@ function Listener() {
         btns[i].addEventListener('click', (x) => {
             document.getElementById('lable').innerHTML = `Search ${x.target.innerHTML}`
             let aPI = x.target.value
-            serchvalue.name=aPI
+            serchvalue.name = aPI
             detailsSection.innerHTML = ""
-            serchvalue.value=""
+            serchvalue.value = ""
             getApiData(aPI)
             search()
         })
@@ -245,7 +262,6 @@ function Listener() {
 }
 
 // Taking search values 
-
 const search = () => {
     formBtn.addEventListener('click', (e) => {
         e.preventDefault()
@@ -256,7 +272,7 @@ const search = () => {
 
 // Flecting search values and calling functions to display results on DOM
 const serchResult = (x) => {
-    let y=serchvalue.name
+    let y = serchvalue.name
     console.log(y)
     fetch(`${y}`, {
         method: 'GET',
@@ -286,29 +302,27 @@ const serchResult = (x) => {
                         foundResult.push(j[i])
                         populateLawEnforcements(j[i])
                     }
-                }else if (y === UnemploymentRates) {
+                } else if (y === UnemploymentRates) {
                     if (j[i].area_name === x) {
                         foundResult.push(j[i])
                         populateUnemploymentRates(j[i])
                     }
-                }else if (y === mOjobCenters) {
+                } else if (y === mOjobCenters) {
                     if (j[i].city === x) {
                         foundResult.push(j[i])
                         populateJobCenters(j[i])
                     }
-                }else if (y === countiess) {
+                } else if (y === countiess) {
                     if (j[i].name === x) {
                         foundResult.push(j[i])
                         populateCounties(j[i])
                     }
-                }else if (y === ZipcodeDatas) {
+                } else if (y === ZipcodeDatas) {
                     if (j[i].zcta5ce === x) {
                         foundResult.push(j[i])
                         populateZips(j[i])
                     }
                 }
-
-                
             }
             if (foundResult.length === 0) {
                 let h3list = document.createElement('h3'),
@@ -322,9 +336,7 @@ const serchResult = (x) => {
         })
 }
 
-
 //Starting function
 document.addEventListener('DOMContentLoaded', function () {
     getApiData(GovernmentJob)
 })
-
